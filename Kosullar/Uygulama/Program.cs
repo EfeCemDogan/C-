955820796acff286cea9b1f1407be3b076cad8d6
Efe Cem Dogan <125884274+EfeCemDogan@users.noreply.github.com>
2023-09-02 16:31:48 +0300

    C#

---
+++ Kosullar/Uygulama/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Globalization;
+using System.Linq.Expressions;
+using System.Threading.Tasks.Dataflow;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.Write("ad: ");
+            // string ad = Console.ReadLine();
+
+            // Console.Write("yas: ");
+            // int yas = int.Parse(Console.ReadLine());
+
+            // Console.Write("Eğitim: ");
+            // string egitim = Console.ReadLine();
+
+            // if (yas>=18)  {
+            //     if (egitim=="lise" || egitim=="üniversite")
+            //     {
+            //     Console.WriteLine(ad +" ehliyet alabilir");
+            //     }
+            //     else {
+            //     Console.WriteLine(ad +" ehliyet alamanız için eğitim durumunuz yetersiz");
+            //     }
+            // }
+            // else {
+            //     Console.WriteLine(ad +" ehliyet almanız için yaşınız yetersiz");
+            // }
+
+            // Console.Write("sayi1: ");
+            // int sayi1 = int.Parse(Console.ReadLine());
+
+            // Console.Write("sayi2: ");
+            // int sayi2 = int.Parse(Console.ReadLine());
+
+            // Console.Write("ssayi3: ");
+            // int sayi3 = int.Parse(Console.ReadLine());
+
+            // if ((sayi1>sayi2) && (sayi1>sayi3)) {
+            //     Console.WriteLine("en büyük: " +sayi1);
+            // }
+
+            // else if ((sayi2>sayi3) && (sayi2>sayi1)) {
+            //     Console.WriteLine("en büyük: " +sayi2);
+            // }
+            // else if ((sayi3>sayi2) && (sayi3>sayi1)) {
+            //     Console.WriteLine("en büyük: " +sayi3);
+            // }
+            // else {
+            //     Console.WriteLine("geçersiz bir sayı girdiniz");
+            // }
+
+
+            // Console.Write("vize1: ");
+            // int vize1 = int.Parse(Console.ReadLine());
+
+            // Console.Write("vize2: ");
+            // int vize2 = int.Parse(Console.ReadLine());
+
+            // Console.Write("final: ");
+            // int final = int.Parse(Console.ReadLine());
+
+            // int ortalama = ((vize1+vize2+final)/3);
+
+            // if (ortalama >= (0) && ortalama <=(24)) {
+            //     Console.WriteLine ($"Ortalamanız: {ortalama}  Notunuz: 0 dersten kaldınız.");
+            // }
+            // else if (ortalama >= (25) && ortalama <=(44)) {
+            //     Console.WriteLine ($"Ortalamanız: {ortalama}  Notunuz: 1 dersten kaldınız.");
+            // }
+            // else if (ortalama >= (45) && ortalama <=(54)) {
+            //     Console.WriteLine ($"Ortalamanız: {ortalama}  Notunuz: 2 dersten geçtiniz.");
+            // }
+            // else if (ortalama >= (55) && ortalama <=(69)) {
+            //     Console.WriteLine ($"Ortalamanız: {ortalama}  Notunuz: 3 dersten geçtiniz.");
+            // }
+            // else if (ortalama >= (70) && ortalama <=(84)) {
+            //     Console.WriteLine ($"Ortalamanız: {ortalama}  Notunuz: 4 dersten geçtiniz.");
+            // }
+            // else if (ortalama >= (85) && ortalama <=(100)) {
+            //     Console.WriteLine ($"Ortalamanız: {ortalama}  Notunuz: 5 dersten geçtiniz.");
+            // }
+            // else {
+            //     Console.WriteLine ($"Hatalı yazım");
+            // }
+
+
+
+            // Console.Write("Gün: ");
+            // int gun = int.Parse(Console.ReadLine());
+
+            // Console.Write("Ay: ");
+            // int ay = int.Parse(Console.ReadLine());
+
+            // Console.Write("Yıl: ");
+            // int yil = int.Parse(Console.ReadLine());
+
+            // int toplamGun = (DateTime.Now - new DateTime(yil,ay,gun)).Days;
+
+            // if (toplamGun<=365) {
+            //     Console.WriteLine("1.bakım");
+            // }
+            // else if ((toplamGun>=365) && (toplamGun<=365*2)) {
+            //     Console.WriteLine("2.bakım");
+            // }
+            // else if (toplamGun>=365*2) {
+            //     Console.WriteLine("3.bakım");
+            // }
+            // else {
+            //     Console.WriteLine("Hatalı Giriş Yaptınız.");
+            // }
+
+
+
+
+
+        }
+    }
+}
