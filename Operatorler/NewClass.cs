3abaed23f14887b62fa5c7e30e118c2fa9085a18
Efe Cem Dogan <125884274+EfeCemDogan@users.noreply.github.com>
2024-01-29 14:13:09 +0300

    C# Microsoft

---
+++ Operatorler/NewClass.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft
+{
+    public class NewClass
+    {
+        string[] names = { "Rowena", "Robin", "Bao" };
+        foreach (string name in names)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
