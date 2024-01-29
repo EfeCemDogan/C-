f16ffb4e317df3bfe957090dc1a1122ed855f2ea
Efe Cem Dogan <125884274+EfeCemDogan@users.noreply.github.com>
2024-01-29 20:44:49 +0300

    C# Microsoft

--- Microsoft/Program.cs
+++ Microsoft/Program.cs
@@ -1240,26 +1240,404 @@ namespace ConsoleApp
             //     }
             // }
 
+            // static void Main() {
+            //    /* 
+            //     This code uses a names array and corresponding methods to display
+            //     greeting messages
+            //     */
+
+            //     string[] names = new string[] { "Sophia", "Andrew", "AllGreetings" };
+
+            //     string messageText = "";
+
+            //     foreach (string name in names)
+            //     {
+            //         if (name == "Sophia")
+            //             messageText = SophiaMessage();
+            //         else if (name == "Andrew")
+            //             messageText = AndrewMessage();
+            //         else if (name == "AllGreetings")
+            //             messageText = SophiaMessage();
+            //             messageText = messageText + "\n\r" + AndrewMessage();
+
+            //         Console.WriteLine(messageText + "\n\r");
+            //     }
+
+            //     bool pauseCode = true;
+            //     while (pauseCode == true);
+
+            //     static string SophiaMessage()
+            //     {
+            //         return "Hello, my name is Sophia.";
+            //     }
+
+            //     static string AndrewMessage()
+            //     {
+            //         return "Hi, my name is Andrew. Good to meet you.";
+            //     }
+            // }
+
+            // static void Main() {
+            //    /* 
+            //     This code uses a names array and corresponding methods to display
+            //     greeting messages
+            //     */
+
+            //     string[] names = new string[] { "Sophia", "Andrew", "AllGreetings" };
+
+            //     string messageText = "";
+
+            //     foreach (string name in names)
+            //     {
+            //         if (name == "Sophia")
+            //             messageText = SophiaMessage();
+            //         else if (name == "Andrew")
+            //             messageText = AndrewMessage();
+            //         else if (name == "AllGreetings")
+            //             messageText = SophiaMessage();
+            //             messageText = messageText + "\n\r" + AndrewMessage();
+
+            //         Console.WriteLine(messageText + "\n\r");
+            //     }
+
+            //     bool pauseCode = true;
+            //     while (pauseCode == true);
+
+            //     static string SophiaMessage()
+            //     {
+            //         return "Hello, my name is Sophia.";
+            //     }
+
+            //     static string AndrewMessage()
+            //     {
+            //         return "Hi, my name is Andrew. Good to meet you.";
+            //     }
+            // }
+
+            // static void Main() {
+            //     string? readResult;
+            //     int startIndex = 0;
+            //     bool goodEntry = false;
+
+            //     int[] numbers = { 1, 2, 3, 4, 5 };
+
+            //     // Display the array to the console.
+            //     Console.Clear();
+            //     Console.Write("\n\rThe 'numbers' array contains: { ");
+            //     foreach (int number in numbers)
+            //     {
+            //         Console.Write($"{number} ");
+            //     }
+
+            //     // To calculate a sum of array elements, 
+            //     //  prompt the user for the starting element number.
+            //     Console.WriteLine($"}}\n\r\n\rTo sum values 'n' through 5, enter a value for 'n':");
+            //     while (goodEntry == false)
+            //     {
+            //         readResult = Console.ReadLine();
+            //         goodEntry = int.TryParse(readResult, out startIndex);
+
+            //         if (startIndex > 5)
+            //         {
+            //             goodEntry = false;
+            //             Console.WriteLine("\n\rEnter an integer value between 1 and 5");
+            //         }
+            //     }
+
+            //     // Display the sum and then pause.
+            //     Console.WriteLine($"\n\rThe sum of numbers {startIndex} through {numbers.Length} is: {SumValues(numbers, startIndex)}");
+
+            //     Console.WriteLine("press Enter to exit");
+            //     readResult = Console.ReadLine();
+
+            //     // This method returns the sum of elements n through 5
+            //     static int SumValues(int[] numbers, int n)
+            //     {
+            //         int sum = 0;
+            //         for (int i = n; i < numbers.Length; i++)
+            //         {
+            //             sum += numbers[i];
+            //         }
+            //         return sum;
+            //     }
+            // }
+
+            // static void Main() {
+            //    try
+            //     {
+            //         Process1();
+            //     }
+            //     catch
+            //     {
+            //         Console.WriteLine("An exception has occurred");
+            //     }
+
+            //     Console.WriteLine("Exit program");
+
+            //     static void Process1()
+            //     {
+            //         WriteMessage();
+            //     }
+
+            //     static void WriteMessage()
+            //     {
+            //         double float1 = 3000.0;
+            //         double float2 = 0.0;
+            //         int number1 = 3000;
+            //         int number2 = 0;
+
+            //         Console.WriteLine(float1 / float2);
+            //         Console.WriteLine(number1 / number2);
+            //     }
+            // }
+
+            // static void Main() {
+            //    try
+            //     {
+            //         int num1 = int.MaxValue;
+            //         int num2 = int.MaxValue;
+            //         int result = num1 + num2;
+            //         Console.WriteLine("Result: " + result);
+
+            //         string str = null;
+            //         int length = str.Length;
+            //         Console.WriteLine("String Length: " + length);
+
+            //         int[] numbers = new int[5];
+            //         numbers[5] = 10;
+            //         Console.WriteLine("Number at index 5: " + numbers[5]);
+
+            //         int num3 = 10;
+            //         int num4 = 0;
+            //         int result2 = num3 / num4;
+            //         Console.WriteLine("Result: " + result2);
+            //     }
+            //     catch (OverflowException ex)
+            //     {
+            //         Console.WriteLine("Error: The number is too large to be represented as an integer." + ex.Message);
+            //     }
+            //     catch (NullReferenceException ex)
+            //     {
+            //         Console.WriteLine("Error: The reference is null." + ex.Message);
+            //     }
+            //     catch (IndexOutOfRangeException ex)
+            //     {
+            //         Console.WriteLine("Error: Index out of range." + ex.Message);
+            //     }
+            //     catch (DivideByZeroException ex)
+            //     {
+            //         Console.WriteLine("Error: Cannot divide by zero." + ex.Message);
+            //     }
+
+            //     Console.WriteLine("Exiting program.");
+            // }
+
+
+            // static void Main() {
+            //     checked
+            //         {
+            //             try
+            //             {
+            //                 int num1 = int.MaxValue;
+            //                 int num2 = int.MaxValue;
+            //                 int result = num1 + num2;
+            //                 Console.WriteLine("Result: " + result);
+            //             }
+            //             catch (OverflowException ex)
+            //             {
+            //                 Console.WriteLine("Error: The number is too large to be represented as an integer. " + ex.Message);
+            //             }
+            //         }
+
+            //         try
+            //         {
+            //             string? str = null;
+            //             int length = str.Length;
+            //             Console.WriteLine("String Length: " + length);
+            //         }
+            //         catch (NullReferenceException ex)
+            //         {
+            //             Console.WriteLine("Error: The reference is null. " + ex.Message);
+            //         }
+
+            //         try
+            //         {
+            //             int[] numbers = new int[5];
+            //             numbers[5] = 10;
+            //             Console.WriteLine("Number at index 5: " + numbers[5]);
+            //         }
+            //         catch (IndexOutOfRangeException ex)
+            //         {
+            //             Console.WriteLine("Error: Index out of range. " + ex.Message);
+            //         }
+
+            //         try
+            //         {
+            //             int num3 = 10;
+            //             int num4 = 0;
+            //             int result2 = num3 / num4;
+            //             Console.WriteLine("Result: " + result2);
+            //         }
+            //         catch (DivideByZeroException ex)
+            //         {
+            //             Console.WriteLine("Error: Cannot divide by zero. " + ex.Message);
+            //         }
+
+            //         Console.WriteLine("Exiting program.");
+            // }
+
+
+            // static void Main() {
+            //     try
+            //     {
+            //         OperatingProcedure1();
+            //     }
+            //     catch (Exception ex)
+            //     {
+            //         Console.WriteLine(ex.Message);
+            //         Console.WriteLine("Exiting application.");
+            //     }
+
+            //     static void OperatingProcedure1()
+            //     {
+            //         string[][] userEnteredValues = new string[][]
+            //         {
+            //             new string[] { "1", "two", "3"},
+            //             new string[] { "0", "1", "2"}
+            //         };
+
+            //         foreach(string[] userEntries in userEnteredValues)
+            //         {
+            //             try
+            //             {
+            //                 BusinessProcess1(userEntries);
+            //             }
+            //             catch (Exception ex)
+            //             {
+            //                 if (ex.StackTrace.Contains("BusinessProcess1"))
+            //                 {
+            //                     if (ex is FormatException)
+            //                     {
+            //                         Console.WriteLine(ex.Message);
+            //                         Console.WriteLine("Corrective action taken in OperatingProcedure1");
+            //                     }
+            //                     else if (ex is DivideByZeroException)
+            //                     {
+            //                         Console.WriteLine(ex.Message);
+            //                         Console.WriteLine("Partial correction in OperatingProcedure1 - further action required");
+
+            //                         // re-throw the original exception
+            //                         throw;
+            //                     }
+            //                     else
+            //                     {
+            //                         // create a new exception object that wraps the original exception
+            //                         throw new ApplicationException("An error occurred - ", ex);
+            //                     }
+            //                 }
+            //             }
+
+            //         }
+            //     }
+
+            //     static void BusinessProcess1(String[] userEntries)
+            //     {
+            //         int valueEntered;
+
+            //         foreach (string userValue in userEntries)
+            //         {
+            //             try
+            //             {
+            //                 valueEntered = int.Parse(userValue);
+
+            //                 checked
+            //                 {
+            //                     int calculatedValue = 4 / valueEntered;
+            //                 }
+            //             }
+            //             catch (FormatException)
+            //             {
+            //                 FormatException invalidFormatException = new FormatException("FormatException: User input values in 'BusinessProcess1' must be valid integers");
+            //                 throw invalidFormatException;
+            //             }
+            //             catch (DivideByZeroException)
+            //             {
+            //                 DivideByZeroException unexpectedDivideByZeroException = new DivideByZeroException("DivideByZeroException: Calculation in 'BusinessProcess1' encountered an unexpected divide by zero");
+            //                 throw unexpectedDivideByZeroException;
+
+            //             }
+            //         }
+            //     }
+            // }
+
             static void Main() {
-               
+                string[][] userEnteredValues = new string[][]
+                {
+                            new string[] { "1", "2", "3"},
+                            new string[] { "1", "two", "3"},
+                            new string[] { "0", "1", "2"}
+                };
+
+                try
+                {
+                    Workflow1(userEnteredValues);
+                    Console.WriteLine("'Workflow1' completed successfully.");
+
+                }
+                catch (DivideByZeroException ex)
+                {
+                    Console.WriteLine("An error occurred during 'Workflow1'.");
+                    Console.WriteLine(ex.Message);
+                }
+
+                static void Workflow1(string[][] userEnteredValues)
+                {
+                    foreach (string[] userEntries in userEnteredValues)
+                    {
+                        try
+                        {
+                            Process1(userEntries);
+                            Console.WriteLine("'Process1' completed successfully.");
+                            Console.WriteLine();
+                        }
+                        catch (FormatException ex)
+                        {
+                            Console.WriteLine("'Process1' encountered an issue, process aborted.");
+                            Console.WriteLine(ex.Message);
+                            Console.WriteLine();
+                        }
+                    }
+                }
+
+                static void Process1(String[] userEntries)
+                {
+                    int valueEntered;
+
+                    foreach (string userValue in userEntries)
+                    {
+                        bool integerFormat = int.TryParse(userValue, out valueEntered);
+
+                        if (integerFormat == true)
+                        {
+                            if (valueEntered != 0)
+                            {
+                                checked
+                                {
+                                    int calculatedValue = 4 / valueEntered;
+                                }
+                            }
+                            else
+                            {
+                                throw new DivideByZeroException("Invalid data. User input values must be non-zero values.");
+                            }
+                        }
+                        else
+                        {
+                            throw new FormatException("Invalid data. User input values must be valid integers.");
+                        }
+                    }
+                }
             }
-
-            // static void Main() {
-               
-            // }
-
-            // static void Main() {
-               
-            // }
-
-            // static void Main() {
-               
-            // }
-
-            // static void Main() {
-               
-            // }
-
 
         }
 }
