d29ed9bf54cccdebc033c3a24f1dc05ecf5085e1
Efe Cem Dogan <125884274+EfeCemDogan@users.noreply.github.com>
2024-01-29 18:58:53 +0300

    C# Microsoft

--- Microsoft/Program.cs
+++ Microsoft/Program.cs
@@ -969,14 +969,124 @@ namespace ConsoleApp
 
 
             // static void Main() {
-               
+            //    string[,] corporate = 
+            //     {
+            //         {"Robert", "Bavin"}, {"Simon", "Bright"},
+            //         {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+            //         {"Sarah", "Delucchi"}, {"Sinan", "Ali"}
+            //     };
+
+            //     string[,] external = 
+            //     {
+            //         {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+            //         {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+            //     };
+
+            //     string externalDomain = "hayworth.com";
+
+            //     for (int i = 0; i < corporate.GetLength(0); i++) 
+            //     {
+            //         // display internal email addresses
+            //     }
+
+            //     for (int i = 0; i < external.GetLength(0); i++) 
+            //     {
+            //         // display external email addresses
+            //     }
             // }
 
+
+            // static void Main() {
+            //     double total = 0;
+            //     double minimumSpend = 30.00;
+
+            //     double[] items = {15.97, 3.50, 12.25, 22.99, 10.98};
+            //     double[] discounts = {0.30, 0.00, 0.10, 0.20, 0.50};
+
+            //     for (int i = 0; i < items.Length; i++)
+            //     {
+            //         total += GetDiscountedPrice(i);
+            //     }
+
+            //     total -= TotalMeetsMinimum() ? 5.00 : 0.00;
+
+            //     Console.WriteLine($"Total: ${FormatDecimal(total)}");
+
+            //     double GetDiscountedPrice(int itemIndex)
+            //     {
+            //         return items[itemIndex] * (1 - discounts[itemIndex]);
+            //     }
+
+            //     bool TotalMeetsMinimum()
+            //     {
+            //         return total >= minimumSpend;
+            //     }
+
+            //     string FormatDecimal(double input)
+            //     {
+            //         return input.ToString().Substring(0, 5);
+            //     }
+            // }
+
+
+            // static void Main() {
+            //     int target = 30;
+            //     int[] coins = new int[] {5, 5, 50, 25, 25, 10, 5};
+            //     int[,] result = TwoCoins(coins, target);
+
+            //     if (result.Length == 0) 
+            //     {
+            //         Console.WriteLine("No two coins make change");
+            //     } 
+            //     else 
+            //     {
+            //         Console.WriteLine("Change found at positions:");
+            //         for (int i = 0; i < result.GetLength(0); i++) 
+            //         {
+            //             if (result[i,0] == -1) 
+            //             {
+            //                 break;
+            //             }
+            //             Console.WriteLine($"{result[i,0]},{result[i,1]}");
+            //         }
+            //     }
+
+            //     int[,] TwoCoins(int[] coins, int target) 
+            //     {
+            //         int[,] result = {{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1}};
+            //         int count = 0;
+
+            //         for (int curr = 0; curr < coins.Length; curr++) 
+            //         {
+            //             for (int next = curr + 1; next < coins.Length; next++) 
+            //             {    
+            //                 if (coins[curr] + coins[next] == target) 
+            //                 {
+            //                     result[count, 0] = curr;
+            //                     result[count, 1] = next;
+            //                     count++;
+            //                 }
+            //                 if (count == result.GetLength(0)) 
+            //                 {
+            //                     return result;
+            //                 }
+            //             }
+            //         }
+            //         return (count == 0) ? new int[0,0] : result;
+            //     }
+            // }
 
             // static void Main() {
                
             // }
 
+            // static void Main() {
+               
+            // }
+
+            // static void Main() {
+               
+            // }
 
             // static void Main() {
                
